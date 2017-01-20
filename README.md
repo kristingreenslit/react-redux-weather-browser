19fb6332211b1bf1838b5c0499d7319459a8ddd2
Kristin Greenslit <kristingreenslit@gmail.com>
2017-01-19 20:44:52 -0500

    create search bar event handlers

--- README.md
+++ README.md
@@ -1,25 +1,15 @@
-# ReduxSimpleStarter
+# React/Redux weather browser
 
-Interested in learning [Redux](https://www.udemy.com/react-redux/)?
+### Starter code: https://github.com/StephenGrider/ReduxSimpleStarter
 
-###Getting Started###
+#### Getting Started
 
-There are two methods for getting started with this repo.
-
-####Familiar with Git?#####
-Checkout this repo, install dependencies, then start the gulp process with the following:
+Clone repo, install dependencies, then start the server:
 
 ```
-	> git clone git@github.com:StephenGrider/ReduxSimpleStarter.git
-	> cd ReduxSimpleStarter
+	> git clone git@github.com:kristingreenslit/react-redux-weather-browser.git
+	> cd react-redux-weather-browser
 	> npm install
 	> npm start
 ```
 
-####Not Familiar with Git?#####
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
-	> npm install
-	> npm start
-```
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+    // needed b/c value of 'this' changes in between {this.onInputChange} and actual function
+    // binding SearchBar to onInputChange so it's passed correctly
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
 	render() {
 		return (
-			<form className="input-group">
-        <input />
+			<form onSubmit={this.onFormSubmit} className="input-group">
+        <input 
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange} />
         <span className="input-group-btn">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
