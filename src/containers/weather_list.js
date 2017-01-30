f758812e9a79c94ec15e3f5b7bed24cb5a162290
Kristin Greenslit <kristingreenslit@gmail.com>
2017-01-29 22:30:54 -0500

    populate table with city names

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -4,6 +4,16 @@ import { connect } from 'react-redux';
 // removed b/c no longer exporting just WeatherList as the default
 // export default class WeatherList extends Component {
 class WeatherList extends Component {
+  
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
 	render() {
 		return (
       <table className="table table-hover">
@@ -16,7 +26,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
 		);
